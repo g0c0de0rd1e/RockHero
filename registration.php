506ae3e43895c34ed23db706e932166fd891a216
Valera <kqlykjk@gmail.com>
2024-07-14 18:36:12 +0300

    Added front for login form

--- registration.php
+++ registration.php
@@ -14,81 +14,77 @@
 </head>
 <body>
     <form class="registration-form" action="auth/registration.php" method="post">
-        <?php if (hasMessage('error')){
-            echo getMessage('error');
-        }
-        ?>
-    <div class="wrapper">
-        <h2>Registration</h2>
-        <?php if (hasMessage('error')){
-            echo getMessage('error');
-        } ?>
-        <div class="input-box">
-            <label for="name">
-                Имя
-                <input 
-                type="text" 
-                id="name" 
-                name="name" 
-                placeholder="Иван Иванович"
-                value="<?php echo old('name')?>"
-                <?php validationErrorAttr('name') ?>
-            >
-            <?php if(validationError('name')): ?>
-                <small><?php validationErrorMessage('name') ?></small>
-            <?php endif;?>  
-        </div>
-        <div class="input-box">
-            <label for="email">
-                E-mail
-                <input 
-                type="text" 
-                id="email"
-                name="email"
-                placeholder="admin@redhat.com"
-                value="<?php echo old('email')?>"
-                <?php validationErrorAttr('email') ?>
-            >
-            <?php if(validationError('email')): ?>
-                <small><?php validationErrorMessage('email') ?></small>
-            <?php endif;?>  
-            </label>
-        </div>
-        <div class="input-box">
-            <label for="password">
-                Пароль
-                <input 
-                    type="password" 
-                    id = "password"
-                    name="password"
-                    placeholder="*******"
-                <?php validationErrorAttr('password') ?>
+        <div class="wrapper">
+            <h2>Registration</h2>
+            <?php if (hasMessage('error')){
+                echo getMessage('error');
+            } ?>
+            <div class="input-box">
+                <label for="name">
+                    Имя
+                    <input 
+                    type="text" 
+                    id="name" 
+                    name="name" 
+                    placeholder="Иван Иванович"
+                    value="<?php echo old('name')?>"
+                    <?php validationErrorAttr('name') ?>
                 >
-                <?php if(validationError('password')): ?>
-                    <small><?php validationErrorMessage('password') ?></small>
+                <?php if(validationError('name')): ?>
+                    <small><?php validationErrorMessage('name') ?></small>
                 <?php endif;?>  
-            </label>
-        </div>
-        <div class="input-box">
-            <label for="confirmPassword">
-            Подтверждение пароля
-                <input 
-                    type="password" 
-                    id = "confirmPassword"
-                    name="confirmPassword"
-                    placeholder="*******"
+            </div>
+            <div class="input-box">
+                <label for="email">
+                    E-mail
+                    <input 
+                    type="text" 
+                    id="email"
+                    name="email"
+                    placeholder="admin@redhat.com"
+                    value="<?php echo old('email')?>"
+                    <?php validationErrorAttr('email') ?>
                 >
-            </label> 
+                <?php if(validationError('email')): ?>
+                    <small><?php validationErrorMessage('email') ?></small>
+                <?php endif;?>  
+                </label>
+            </div>
+            <div class="input-box">
+                <label for="password">
+                    Пароль
+                    <input 
+                        type="password" 
+                        id = "password"
+                        name="password"
+                        placeholder="*******"
+                    <?php validationErrorAttr('password') ?>
+                    >
+                    <?php if(validationError('password')): ?>
+                        <small><?php validationErrorMessage('password') ?></small>
+                    <?php endif;?>  
+                </label>
+            </div>
+            <div class="input-box">
+                <label for="confirmPassword">
+                Подтверждение пароля
+                    <input 
+                        type="password" 
+                        id = "confirmPassword"
+                        name="confirmPassword"
+                        placeholder="*******"
+                    >
+                </label> 
+            </div>
+            <div class="input-box button">
+                <input type="Submit" value="Register Now">
+            </div>
+            <div class="text">
+                <h3>Уже есть аккаунт?
+                    <a href="signin.php">Войти</a>
+                </h3>
+            </div>
         </div>
-        <div class="input-box button">
-            <input type="Submit" value="Register Now">
-        </div>
-        <div class="text">
-            <h3>Already have an account?
-                <a href="signin.php">Sign In</a>
-            </h3>
-        </div>
-    </div>
     </form>
 </body>
 </html>
--- signin.php
+++ signin.php
@@ -10,31 +10,46 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>RockHero</title>
+    <link rel="stylesheet" href="css/login.css">
 </head>
 <body>
     <form class="signinform" action="auth/login.php" method="post">
-        <label for="email">
-            Email
-            <input
-                type="text"
-                id="email"
-                name="email"
-                placeholder="email@email.com"
-                value="<?php echo old('email')?>"
-            <?php validationErrorAttr('email') ?>
-        >
-        <?php if(validationError('email')): ?>
-            <small><?php validationErrorMessage('email') ?></small>
-        <?php endif;?>  
-        </label>
-        <label for="password">
-            Пароль
-            <input 
-            type="password" 
-            id="password"
-            name="password"
-            placeholder="********">
-        </label>
-        <input type="submit" value="Login">
+        <div class="wrapper">
+            <div class="input-box">
+                <label for="email">
+                    Email
+                    <input
+                        type="text"
+                        id="email"
+                        name="email"
+                        placeholder="email@email.com"
+                        value="<?php echo old('email')?>"
+                    <?php validationErrorAttr('email') ?>
+                >
+                <?php if(validationError('email')): ?>
+                    <small><?php validationErrorMessage('email') ?></small>
+                <?php endif;?>  
+                </label>
+            </div>
+            <div class="input-box">
+                <label for="password">
+                    Пароль
+                    <input 
+                    type="password" 
+                    id="password"
+                    name="password"
+                    placeholder="********">
+                </label>
+            </div>
+            <div class="input-box button">
+                <input type="Submit" value="Login Now">
+            </div>
+            <div class="text">
+                <h3>Нет аккаунта?
+                    <a href="signin.php">Зарегестрироваться</a>
+                </h3>
+            </div>
+        </div>
+    </form>
 </body>
 </html>
