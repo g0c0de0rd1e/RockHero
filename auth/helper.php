0ea1d12bb84a02a8f207afeb648e46a8a6f1dbcf
Valera <kqlykjk@gmail.com>
2024-07-14 15:45:14 +0300

    Added DB, Full registration Form

--- auth/helper.php
+++ auth/helper.php
@@ -28,3 +28,12 @@
         unset($_SESSION['old'][$key]);
         return $value;
     }
+
+
+    function getPDO(): PDO{
+        try{
+            return new \PDO('mysql:host='. DB_HOST .';port='. DB_PORT .';charset=utf8;dbname='. DB_NAME, DB_USERNAME, DB_PASSWORD);
+        } catch (\PDOException $e){
+            die($e->getMessage());
+        }
+    }
--- auth/registration.php
+++ auth/registration.php
@@ -2,10 +2,10 @@
 
     require_once __DIR__ ."/helper.php";
 
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-    $passwordConfirm = $_POST['confirmPassword'];
+    $name = $_POST['name'] ?? null;
+    $email = $_POST['email'] ?? null;
+    $password = $_POST['password'] ?? null;
+    $passwordConfirm = $_POST['confirmPassword'] ?? null;
 
     $_SESSION['validation'] = [];
 
@@ -20,14 +20,32 @@
         addValidationError('email', 'Введена неправильная почта');
     }
 
-    if (!empty($password)){
+    if (empty($password)){
         addValidationError('password', 'Пароль пустой');
     }
 
-    if ($password == $passwordConfirm){
+    if ($password !== $passwordConfirm){
         addValidationError('password', 'Пароли не совпадают');
     }
 
     if (!empty($_SESSION['validation'])){
         header('Location: /registration.php');
     }
+
+    $pdo = getPDO();
+
+    $query = "INSERT INTO users (name, email, password) VALUES (:name, :email, :password)";
+    $params = [
+        'name' => $name,
+        'email' => $email,
+        'password' => password_hash($password, PASSWORD_DEFAULT)
+    ];
+    $stmt = $pdo->prepare($query);
+
+    try{
+        $stmt->execute($params);
+    } catch(\Exception $e){
+        die($e->getMessage());
+    }
+
+    header('Location: /signin.php');
